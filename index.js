3f00c1a6cae1ba9190f6e2fc738d569d7f0bb647
RuthKibos <kibosruth@gmail.com>
2024-04-15 09:05:18 +0300

    Completed the Lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const input = document.getElementById('button');
+
 function addingEventListener() {
+  input.addEventListener('click', clickAlert);
 }
+
+function clickAlert() {
+  alert("Click Me!");
+}
+
+addingEventListener();
